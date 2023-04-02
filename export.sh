52114274cb425d219c484c8c316ca0117de800df
Zamburinhas <129691434+Zamburinhas@users.noreply.github.com>
2023-04-02 22:55:15 +0200

    Add files via upload

---
+++ export.sh
@@ -0,0 +1,3 @@
+mongoexport --db baseDatosZamburinhas --collection estacionnivels --out estacionesNivel.json
+mongoexport --db baseDatosZamburinhas --collection estacioninfos --out estacionesInfo.json
+mongoexport --db baseDatosZamburinhas --collection estaciontemps --out estacionesTemp.json 
